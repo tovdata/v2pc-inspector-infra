75280a82eedfc1cba3f6f19f25c6af05f7164592
hmin01 <goals1003@gmail.com>
2022-01-19 17:56:21 +0900

    2022.01.19. - Add configuration file for amazon s3 and amazon sqs - Implement class for s3 bucket and sqs queue

---
+++ resources/s3.ts
@@ -0,0 +1,51 @@
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+// Util
+import { createHashId } from "../utils/util";
+
+export class Bucket {
+  private _bucket: s3.CfnBucket;
+  private _scope: Construct;
+
+  /**
+   * Create the cloudFormation resource for bucket
+   * @param scope scope context
+   * @param config configuration for bucket
+   */
+  constructor(scope: Construct, config: any) {
+    this._scope = scope;
+    // Set the cros rules
+    const corsRules: s3.CfnBucket.CorsRuleProperty[] = config.CORSRules !== undefined ? config.CORSRules.map((elem: any): s3.CfnBucket.CorsRuleProperty => { return { allowedMethods: elem.AllowedMethods, allowedHeaders: elem.AllowedHeaders, allowedOrigins: elem.AllowedOrigins, exposedHeaders: elem.ExposedHeaders, maxAge: elem.MaxAgeSeconds }; }) : [];
+    // Create the properties for bucket [Ref. https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-properties-s3-bucket.html]
+    const props: s3.CfnBucketProps = {
+      analyticsConfigurations: config.AnalyticsConfigurationList !== undefined ? Object.keys(config.AnalyticsConfigurationList).length > 0 ? config.AnalyticsConfigurationList : undefined : undefined,
+      bucketName: config.Name,
+      corsConfiguration: corsRules.length > 0 ? { corsRules: corsRules } : undefined,
+      metricsConfigurations: config.MetricsConfigurationList !== undefined ? Object.keys(config.MetricsConfigurationList).length > 0 ? config.MetricsConfigurationList : undefined : undefined,
+      intelligentTieringConfigurations: config.IntelligentTieringConfigurationList !== undefined ? Object.keys(config.IntelligentTieringConfigurationList).length > 0 ? config.IntelligentTieringConfigurationList : undefined : undefined,
+      inventoryConfigurations: config.InventoryConfigurationList !== undefined ? Object.keys(config.InventoryConfigurationList).length > 0 ? config.InventoryConfigurationList : undefined : undefined,
+      ownershipControls: config.OwnershipControls,
+      publicAccessBlockConfiguration: Object.keys(config.PublicAccessBlockConfiguration).length > 0 ? config.PublicAccessBlockConfiguration : undefined,
+      versioningConfiguration: config.Versioning !== undefined ? Object.keys(config.Versioning).length > 0 ? config.Versioning : undefined : undefined,
+      websiteConfiguration: config.BucketWebsite !== undefined ? Object.keys(config.BucketWebsite).length > 0 ? config.BucketWebsite : undefined : undefined
+    };
+    // Create the bucket
+    this._bucket = new s3.CfnBucket(this._scope, createHashId(JSON.stringify(props)), props);
+  }
+
+  /**
+   * Get an arn for bucket
+   * @returns arn for bucket
+   */
+  public getArn(): string {
+    return this._bucket.attrArn;
+  }
+
+  /**
+   * Get a name for bucket
+   * @returns name for bucket
+   */
+  public getName(): string {
+    return this._bucket.ref;
+  }
+}
---
+++ resources/sqs.ts
@@ -0,0 +1,93 @@
+import { Construct } from "constructs";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+// Util
+import { createHashId } from "../utils/util";
+
+export class Queue {
+  private _queue: sqs.CfnQueue;
+  private _scope: Construct;
+
+  /**
+   * Create the cloudFormation resource for queue
+   * @param scope scope context
+   * @param config configuration for queue
+   */
+  constructor(scope: Construct, config: any) {
+    this._scope = scope;
+    // Extract the attributes and set a list of tag
+    const attributes: any = config.Attributes;
+    const tags: any[] = config.Tags !== undefined ? Object.keys(config.Tags).map((key: string) => { return {  key: key, value: config.Tags[key] }; }) : [];
+    // Extract queue name
+    const splitArn: string[] = attributes.QueueArn.split(":");
+    const queueName: string = splitArn[splitArn.length - 1];
+    // Create the properties for queue [Ref. https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-properties-sqs-queues.html]
+    const props: sqs.CfnQueueProps = {
+      contentBasedDeduplication: attributes.FifoQueue === "true" ? attributes.ContentBasedDeduplication : undefined,
+      deduplicationScope: attributes.FifoQueue === "true" ? attributes.DeduplicationScope : undefined,
+      delaySeconds: attributes.DelaySeconds !== undefined ? Number(attributes.DelaySeconds) : undefined,
+      fifoQueue: attributes.FifoQueue === "true" ? attributes.FifoQueue : undefined,
+      fifoThroughputLimit: attributes.FifoQueue === "true" ? attributes.FifoThroughputLimit : undefined,
+      kmsDataKeyReusePeriodSeconds: attributes.KmsDataKeyReusePeriodSeconds !== undefined ? Number(attributes.KmsDataKeyReusePeriodSeconds) : undefined,
+      kmsMasterKeyId: attributes.KmsMasterKeyId,
+      maximumMessageSize: attributes.MaximumMessageSize !== undefined ? Number(attributes.MaximumMessageSize) : undefined,
+      messageRetentionPeriod: attributes.MessageRetentionPeriod !== undefined ? Number(attributes.MessageRetentionPeriod) : undefined,
+      queueName: queueName,
+      receiveMessageWaitTimeSeconds: attributes.ReceiveMessageWaitTimeSeconds !== undefined ? Number(attributes.ReceiveMessageWaitTimeSeconds) : undefined,
+      tags: tags.length > 0 ? tags : undefined,
+      visibilityTimeout: attributes.VisibilityTimeout !== undefined ? Number(attributes.VisibilityTimeout) : undefined
+    };
+    // Create the queue
+    this._queue = new sqs.CfnQueue(this._scope, createHashId(JSON.stringify(props)), props);
+  }
+
+  /**
+   * Get an arn for queue
+   * @returns arn for queue
+   */
+  public getArn(): string {
+    return this._queue.attrArn;
+  }
+
+  /**
+   * Get a name for queue
+   * @returns name for queue
+   */
+  public getName(): string {
+    return this._queue.attrQueueName;
+  }
+
+  /**
+   * Get an url for queue
+   * @returns url for queue
+   */
+  public getUrl(): string {
+    return this._queue.ref;
+  }
+
+  /**
+   * Set the policy for queue
+   * @param config configuration policy
+   */
+  public setPolicy(config: any): void {
+    // Set statement in policy document
+    const statement: any[] = [];
+    for (const elem of config.Statement) {
+      statement.push({
+        Action: elem.Action,
+        Effect: elem.Effect,
+        Principal: elem.Principal,
+        Resource: this._queue.attrArn
+      });
+    }
+    // Create the properties for policy [Ref. https://docs.aws.amazon.com/ko_kr/AWSCloudFormation/latest/UserGuide/aws-resource-sqs-queuepolicy.html]
+    const props: sqs.CfnQueuePolicyProps = {
+      policyDocument: {
+        Version: config.Version,
+        Statement: statement
+      },
+      queues: [this._queue.ref]
+    }
+    // Create the policy for queue
+    new sqs.CfnQueuePolicy(this._scope, createHashId(JSON.stringify(props)), props);
+  }
+}
